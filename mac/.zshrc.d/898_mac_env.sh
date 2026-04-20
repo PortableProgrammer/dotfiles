69906fa883ebe87b458d2424eb3ead801b1115dc
PortableProgrammer <nick@warnerheavyindustries.com>
2026-04-20 16:58:53 -0700

    SSH Agent changes

--- mac/.zshrc.d/898_mac_env.sh
+++ mac/.zshrc.d/898_mac_env.sh
@@ -1,2 +1,9 @@
 # Define 1Password ssh-agent SOCK variable
 export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+
+export PATH="$HOME/.local/bin:$PATH"
+# The following lines have been added by Docker Desktop to enable Docker CLI completions.
+fpath=(/Users/nwarner/.docker/completions $fpath)
+autoload -Uz compinit
+compinit
+# End of Docker CLI completions
